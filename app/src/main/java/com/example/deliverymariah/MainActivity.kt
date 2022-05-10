1fc3a5dc6a7a1cca3f9f5f1229f920127bf4af40
Madesita <mherrera@deuna.io>
2022-05-10 17:45:34 -0500

    avance con metodo de boton

--- app/src/main/java/com/example/deliverymariah/MainActivity.kt
+++ app/src/main/java/com/example/deliverymariah/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.deliverymariah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.deliverymariah.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var views: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        views= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        initializacionListeners()
+    }
+    private fun initializacionListeners()
+    {
+        views.button.setOnClickListener {
+            Toast.makeText( this,resources.getString(R.string.user)+ ": "+
+                    views.editTextTextPersonName.text + "\n"+resources.getString(R.string.password)+": "+views.editTextTextPassword.text+"",
+                  Toast.LENGTH_SHORT).show()
+           // Toast.makeText( this,resources.getString(R.string.password)+ ": "+
+          // views.editTextTextPassword.text, Toast.LENGTH_SHORT).show()
+
+                  }
     }
 }
