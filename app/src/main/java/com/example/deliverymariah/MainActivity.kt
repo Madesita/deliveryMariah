6c0d451831f11491eefdb952ec7eeba1f1b806b5
Madesita <mherrera@deuna.io>
2022-05-12 20:18:10 -0500

    avance con metodos materials y pasar a otra pagina

--- app/src/main/java/com/example/deliverymariah/MainActivity.kt
+++ app/src/main/java/com/example/deliverymariah/MainActivity.kt
@@ -1,27 +1,43 @@
 package com.example.deliverymariah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.deliverymariah.databinding.ActivityMainBinding
+import com.example.deliverymariah.interface_user.Principal
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var views: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        views= ActivityMainBinding.inflate(layoutInflater)
+        views = ActivityMainBinding.inflate(layoutInflater)
         setContentView(views.root)
         initializacionListeners()
     }
     private fun initializacionListeners()
     {
-        views.button.setOnClickListener {
-            Toast.makeText( this,resources.getString(R.string.user)+ ": "+
-                    views.editTextTextPersonName.text + "\n"+resources.getString(R.string.password)+": "+views.editTextTextPassword.text+"",
-                  Toast.LENGTH_SHORT).show()
-           // Toast.makeText( this,resources.getString(R.string.password)+ ": "+
-          // views.editTextTextPassword.text, Toast.LENGTH_SHORT).show()
+        views.button.setOnClickListener{
 
-                  }
+            val userText=views.editTextUserName2.text.toString()
+            val passwordText=views.editTextTextPassword2.text.toString()
+
+            userText?.let {
+                passwordText?.let { itp ->
+                    if (it.equals("made") && itp.equals("123"))
+                    {
+                        goToPrincipal(userText)
+                    }
+                }
+            }
+        }
+    }
+    private fun goToPrincipal(userText: String)
+    {
+        var  intent= Intent(this, Principal::class.java)
+        intent.putExtra( "name_user",userText)
+        intent.putExtra( "name_Password",userText)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/deliverymariah/interface_user/Principal.kt
@@ -0,0 +1,25 @@
+package com.example.deliverymariah.interface_user
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deliverymariah.R
+import com.example.deliverymariah.databinding.ActivityPrincipalBinding
+
+class Principal : AppCompatActivity() {
+
+    private lateinit var views: ActivityPrincipalBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        views= ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        showName(intent)
+    }
+    private fun showName(intent: Intent)
+    {
+        var  userName= intent.getStringExtra("name_user")
+        views.name1.setText(userName)
+        var  userPassword= intent.getStringExtra("name_Password")
+        views.name2.setText(userPassword)
+    }
+}
